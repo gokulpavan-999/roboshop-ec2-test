b90dea39a3865cf31c27bd6910c190514003bd1e
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-22 12:17:17 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,6 @@
 module "catalogue" {
   source = "git::https://github.com/gokulpavan-999/terraform-aws-instance.git"
   ami_id = "ami-09c813fb71547fc4f"
+  sg_ids = ["sg-072023e212f530c25"]
+  instance_type = "t3.micro"
 }
