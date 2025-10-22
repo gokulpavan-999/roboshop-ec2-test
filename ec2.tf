a9826ba2a5249b8931b6ac143678b0918095ba6a
gokulpavan-999 <pavanthota480@gmail.com>
2025-10-22 14:07:05 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,30 +1,20 @@
 module "catalogue" {
-  #source = "git::https://github.com/gokulpavan-999/terraform-aws-instance.git"
-  #ami_id = "ami-09c813fb71547fc4f"
-  #sg_ids = ["sg-072023e212f530c25"]
-  #instance_type = "t3.micro"
-  #tags = {
-    #Name = "module-demo"
-  #}
-
-  source = "git::https://github.com/gokulpavan-999/terraform-aws-instance.git"
-  ami_id = var.ami_id
-  sg_ids = var.sg_ids
-  instance_type = var.instance_type
-  tags = var.tags
-
+  source         = "git::https://github.com/gokulpavan-999/terraform-aws-instance.git"
+  ami_id         = var.ami_id
+  sg_ids         = var.sg_ids
+  instance_type  = var.instance_type
+  tags           = var.tags
 }
 
-  output "pub_ip" {
-    value = module.catalogue.public_ip
-  }
+output "pub_ip" {
+  value = module.catalogue.public_ip
+}
 
-  output "priv_ip" {
-    value = module.catalogue.private_ip
-  }
+output "priv_ip" {
+  value = module.catalogue.private_ip
+}
 
-  output "inst_id" {
-    value = module.catalogue.id
-  }
-
+output "inst_id" {
+  value = module.catalogue.instance_id
+}
 
